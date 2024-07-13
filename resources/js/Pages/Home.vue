c9e1f0766382c68e1cc1d5c8fd25a4253bf40a10
aidosgal <agalimzhan928@gmail.com>
2024-07-14 00:28:13 +0500

    fix: background image on the phones

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,10 +1,20 @@
 <template>
-    <div class="w-full h-screen bg-[url('http://localhost/images/backhround.png')] bg-no-repeat bg-cover text-white text-center">
+    <div class="w-full h-screen bg-no-repeat bg-cover text-white text-center" :style="{ backgroundImage: 'url(' + imageUrl + ')' }">
        Home
     </div>
 </template>
+
 <script>
 export default {
-
+    data() {
+        return {
+            imageUrl: 'http://localhost/images/backhround.png'
+        }
+    }
 }
 </script>
+
+<style scoped>
+
+</style>
+
