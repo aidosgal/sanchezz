c93613acd6690f76205a3a0a9a99196230b5778d
aidosgal <agalimzhan928@gmail.com>
2024-07-13 22:45:49 +0500

    feat: test telegraph facade

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -4,11 +4,17 @@ namespace App\Telegram;
 
 use DefStudio\Telegraph\Facades\Telegraph;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use Illuminate\Support\Stringable;
 
 class Handler extends WebhookHandler
 {
     public function start() : void
     {
         Telegraph::message('Привет')->send();
+    }
+
+    protected function handleUnkownCommand(Stringable $text): void
+    {
+        $this->reply('Неизвестная команда');
     }
 }
