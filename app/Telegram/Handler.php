5143342c7feb8d3b926bd41772077572b226cb5f
aidosgal <agalimzhan928@gmail.com>
2024-07-13 22:59:41 +0500

    feat: test telegraph facade

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -4,13 +4,22 @@ namespace App\Telegram;
 
 use DefStudio\Telegraph\Facades\Telegraph;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use DefStudio\Telegraph\Models\TelegraphChat;
 use Illuminate\Support\Stringable;
 
 class Handler extends WebhookHandler
 {
     public function start() : void
     {
-        Telegraph::message('Привет')->send();
+        $chat = $this->chat;
+
+        if ($chat instanceof TelegraphChat) {
+            Telegraph::chat($chat)
+                ->message('Привет')
+                ->send();
+        } else {
+            $this->reply('Chat not found.');
+        }
     }
 
     protected function handleUnknownCommand(Stringable $text): void
