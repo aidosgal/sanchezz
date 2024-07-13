735ffe16675d2166c1a979d5741c79da8566f20c
aidosgal <agalimzhan928@gmail.com>
2024-07-14 01:10:20 +0500

    fix: background image on the phones

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,9 +3,6 @@
         <div class="text-center py-16">
             Home
         </div>
-        <div class="fixed inset-x-0 bottom-0 bg-gray-900 text-white py-4 text-center">
-            Footer content
-        </div>
     </div>
 </template>
 
@@ -21,7 +18,6 @@ export default {
 
 <style>
 body {
-    height: 100%;
     margin: 0;
     padding: 0;
     background: url('https://srv451534.hstgr.cloud/images/backhround.png') no-repeat center center fixed;
