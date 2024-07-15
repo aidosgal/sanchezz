1d7200e7ef0f81aa699b0d063ed3f45975b695f4
aidosgal <agalimzhan928@gmail.com>
2024-07-15 20:39:56 +0500

    feat: add design to login and register

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -1,20 +1,62 @@
 <template>
-    <div class="h-screen bg-[url('https://srv451534.hstgr.cloud/images/register.png')] bg-no-repeat bg-cover bg-center">
-        <div>Login</div>
+  <div class="flex h-screen bg-[url('https://srv451534.hstgr.cloud/images/register.png')] bg-no-repeat bg-cover bg-center">
+    <div class="text-white mx-auto py-20 px-10 w-full">
+      <a href="/" class="block text-2xl font-bold"><</a>
+      <div class="text-center text-4xl font-bold mt-5 bg-clip-text text-transparent bg-gradient-to-r from-[#FFFFFF] via-[#FF7D61] to-[#FF0AC9]">Log in</div>
+      <div class="text-center mt-2">Enter your details <br/>to login into an account</div>
+      <input
+        type="text"
+        v-model="email"
+        class="block w-full mt-5 py-4 rounded bg-[#1E2330] text-white text-center"
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        v-model="password"
+        class="block w-full mt-5 py-4 rounded bg-[#1E2330] text-white text-center"
+        placeholder="Password"
+      />
+      <a href="/register" class="block text-center mt-5 bg-clip-text text-transparent bg-gradient-to-b from-[#FFFFFF] to-[#FFCE50]">I don't have an account</a>
+      <div class="w-full fixed left-0 bottom-20 px-10">
+        <button
+          :class="buttonClasses"
+          class="block text-lg w-full py-3 font-bold mt-5 rounded-full"
+          :disabled="!isFormValid"
+        >
+          Confirm
+        </button>
+      </div>
     </div>
+  </div>
 </template>
+
 <script>
 export default {
-
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  computed: {
+    isFormValid() {
+      return this.email !== '' && this.password !== '';
+    },
+    buttonClasses() {
+      return this.isFormValid
+        ? 'text-white bg-[#0095FF]'
+        : 'text-[#656D85] bg-[#292F40]';
+    }
+  }
 }
 </script>
 
 <style>
 body {
-    margin: 0;
-    padding: 0;
-    overflow-y: hidden;
-    overflow-x: hidden;
-    background-size: cover;
+  margin: 0;
+  padding: 0;
+  overflow-y: hidden;
+  overflow-x: hidden;
+  background-size: cover;
 }
 </style>
--- resources/js/Pages/Register.vue
+++ resources/js/Pages/Register.vue
@@ -1,11 +1,59 @@
 <template>
-    <div class="h-screen bg-[url('https://srv451534.hstgr.cloud/images/register.png')] w-full  bg-no-repeat bg-cover bg-center">
-        <div>Register</div>
+  <div class="flex h-screen bg-[url('https://srv451534.hstgr.cloud/images/register.png')] bg-no-repeat bg-cover bg-center">
+    <div class="text-white mx-auto py-20 px-10 w-full">
+      <a href="/" class="block text-2xl font-bold"><</a>
+      <div class="text-center text-4xl font-bold mt-5 bg-clip-text text-transparent bg-gradient-to-r from-[#FFFFFF] via-[#FF7D61] to-[#FF0AC9]">Registration</div>
+      <div class="text-center mt-2">Enter your details <br/>to create an account</div>
+      <input
+        type="text"
+        v-model="email"
+        class="block w-full mt-5 py-4 rounded bg-[#1E2330] text-white text-center"
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        v-model="password"
+        class="block w-full mt-5 py-4 rounded bg-[#1E2330] text-white text-center"
+        placeholder="Password"
+      />
+      <input
+        type="password"
+        v-model="confirm_password"
+        class="block w-full mt-5 py-4 rounded bg-[#1E2330] text-white text-center"
+        placeholder="Repeat password"
+      />
+      <a href="/login" class="block text-center mt-5 bg-clip-text text-transparent bg-gradient-to-b from-[#FFFFFF] to-[#FFCE50]">I already have an account</a>
+      <div class="w-full fixed left-0 bottom-20 px-10">
+        <button
+          :class="buttonClasses"
+          class="block text-lg w-full py-3 font-bold mt-5 rounded-full"
+          :disabled="!isFormValid"
+        >
+          Confirm
+        </button>
+      </div>
     </div>
+  </div>
 </template>
+
 <script>
 export default {
-
+  data() {
+    return {
+      email: '',
+      password: '',
+      confirm_password: ''
+    }
+  },
+  computed: {
+    isFormValid() {
+      return this.email !== '' && this.password !== '' && this.confirm_password !== '';
+    },
+    buttonClasses() {
+      return this.isFormValid
+        ? 'text-white bg-[#0095FF]'
+        : 'text-[#656D85] bg-[#292F40]';
+    }
+  }
 }
 </script>
-
