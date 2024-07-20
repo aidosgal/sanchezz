d3ec20dc3908c529cac56150837643a9f2df8b7d
aidosgal <agalimzhan928@gmail.com>
2024-07-20 21:08:01 +0500

    fix: web.php error

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Http\Controllers\BalanceController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
@@ -17,21 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    if (Auth::check()) {
-        return redirect()->route('dashboard');
-    }
-    return app(UserController::class);
-})->name('home');
+Route::get('/', [UserController::class, '__invoke'])->name('home');
+
 Route::get('/login', [UserController::class, 'login'])->name('login');
 Route::post('/login', [UserController::class, 'authenticate']);
+
 Route::get('/register', [UserController::class, 'register'])->name('register');
 Route::post('/register', [UserController::class, 'create']);
-Route::get('/profile', [UserController::class, 'profile'])->name('profile');
-Route::post('/update', [UserController::class, 'update']);
+
+Route::get('/profile', [UserController::class, 'profile'])->name('profile')->middleware('auth');
+Route::post('/update', [UserController::class, 'update'])->name('profile.update')->middleware('auth');
 
 Route::get('/dashboard', [HomeController::class, 'home'])->middleware('auth')->name('dashboard');
 
 Route::get('/deposit', [BalanceController::class, 'deposit'])->name('deposit');
-Route::post('/balance/update', [BalanceController::class, 'update']);
-Route::get('/withdraw', [BalanceController::class, 'withdraw'])->name('withdraw');
+Route::post('/balance/update', [BalanceController::class, 'update'])->middleware('auth');
+Route::get('/withdraw', [BalanceController::class, 'withdraw'])->name('withdraw')->middleware('auth');
+
