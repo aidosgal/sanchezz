705ba8998b625e6e9f991cc64db34eef14edde92
aidosgal <agalimzhan928@gmail.com>
2024-07-22 16:11:54 +0500

    feat: open web app expanded

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,18 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     @inertiaHead
     <script>
-        window.Telegram.WebApp.expand();
+        // Define a function to expand the Web App
+        function expandWebApp() {
+            if (window.Telegram && window.Telegram.WebApp) {
+                window.Telegram.WebApp.expand();
+            } else {
+                // Wait for the Telegram WebApp SDK to be loaded
+                setTimeout(expandWebApp, 100); // Retry every 100ms
+            }
+        }
+
+        // Call the function as soon as possible
+        expandWebApp();
     </script>
   </head>
   <body>
