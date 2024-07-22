46e6bcf447808bef5fa825aa243d1f5fbbb878e9
aidosgal <agalimzhan928@gmail.com>
2024-07-22 15:54:23 +0500

    feat: open web app expanded

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,14 @@
     @vite(['resources/js/app.js', 'resources/css/app.css'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     @inertiaHead
+    <script>
+        window.addEventListener('load', () => {
+            if (window.Telegram && window.Telegram.WebApp) {
+                // Expand the Web App to the maximum available height
+                window.Telegram.WebApp.expand();
+            }
+        });
+    </script>
   </head>
   <body>
     @inertia
